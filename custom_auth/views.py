e597cc4d78c8f76357bff1e83b5d893f04a5eca2
kauanAfonso <kauanafon3@gmail.com>
2025-04-08 11:36:29 -0300

    aa

--- custom_auth/views.py
+++ custom_auth/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
-
+from . serializers import ServicoSerializer, AgendamentoSerializer
+from rest_framework.response import Response 
+from rest_framework import status
+from rest_framework.decorators import api_view
+from .models import CustomUser
 # Create your views here.
+
+
+@api_view(['POST'])
+def logar():
+    pass
